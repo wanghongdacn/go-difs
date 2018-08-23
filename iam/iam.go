b613cf44735d10deb9d2c3504b46a635480d78fb
Ruihong <rshi22@flipboard.com>
2018-08-23 13:22:11 +0800

    feat(iam) : add datastore to IAM

--- iam/iam.go
+++ iam/iam.go
@@ -1,8 +1,18 @@
 package iam
 
-import peer "github.com/libp2p/go-libp2p-peer"
+import (
+	ds "gx/ipfs/QmVG5gxteQNEMhrS8prJSmU2C9rebtFuTd3SYZ5kE3YZ5k/go-datastore"
 
+	peer "github.com/libp2p/go-libp2p-peer"
+)
+
+// Message ..
 type Message struct {
 	ID      peer.ID
 	Payload string
+}
+
+// IAM ...
+type IAM struct {
+	datastore ds.Datastore // Local data
 }
