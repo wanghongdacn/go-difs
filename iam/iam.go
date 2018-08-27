e0d0327c8485855dda6198fb2e25dc65ef9ecfb9
Kaili Li <kaili.lee.harold@gmail.com>
2018-08-27 10:40:47 +0800

    iam message add sign/verify

--- iam/iam.go
+++ iam/iam.go
@@ -3,16 +3,27 @@ package iam
 import (
 	ds "gx/ipfs/QmVG5gxteQNEMhrS8prJSmU2C9rebtFuTd3SYZ5kE3YZ5k/go-datastore"
 
-	peer "github.com/libp2p/go-libp2p-peer"
+	"github.com/libp2p/go-libp2p-crypto"
+	"github.com/libp2p/go-libp2p-peer"
 )
 
 // Message ..
 type Message struct {
-	ID      peer.ID
-	Payload string
+	ID        peer.ID
+	Payload   []byte
+	Signature []byte
+	PubKey    crypto.PubKey
 }
 
 // IAM ...
 type IAM struct {
 	datastore ds.Datastore // Local data
+}
+
+func (msg *Message) Verify() {
+
+}
+
+func (msg *Message) Sign(privKey crypto.PrivKey) {
+
 }
